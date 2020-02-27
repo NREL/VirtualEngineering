eab3464390b292c6eed95b2ef01f187b8d593049
Lischeske <jlisches@nrel.gov>
2020-02-27 15:56:28 -0700

    rhs added to bioreactor/wellmixed[..]/solver.cpp
    This needs some implementation of the gamma incomplete function. A quick google search found a couple packages that have this: alglib and boost/math.

--- bioreactor/wellmixed_ODE_solver/solver.cpp
+++ bioreactor/wellmixed_ODE_solver/solver.cpp
@@ -11,12 +11,65 @@ const int B=5;
 
 void get_rhs(std::vector<double>& rhs,std::vector<double> solnvec,double t,int nvars)
 {
-    double lambda=1.0;
+  // JL -- not sure how you want to handle constants. I'm just going to dump them here
+  // ------- MODEL CONSTANTS -------
+  double y_xs = 0.009;
+  double y_as = 1.01;
+  double y_bs = 0.88;
+  double y_os = 0.0467;
 
-    for(int i=0;i<nvars;i++)
-    {
-        rhs[i] = -lambda*solnvec[i];
-    }
+  double x_max = 11;
+  double qs_max = 17;
+  double o2_max = 0.214;
+
+  double K_e = 0.214;
+  double K_s = 31;
+
+  double alpha_s = 3;
+  double beta_s = 12;
+  double alpha_e = 3;
+  double beta_e = 12;
+
+  double kLa = 5;
+
+  // calculate chi_i
+  // FIXME: we need a library for calculating the gamma incomplete function.
+  // Using general "gammainc" for now. In my python code I'm using scipy.special.gammainc
+  // (https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.gammainc.html)
+  // There are a couple libraries that I'm seeing on google, but I don't feel comfortable
+  // making that decision.
+  //
+  // Also, a word of explanation about the form of the gamma incomplete call. This function
+  // is usually expressed as g(a, x), where a and x are obligate positive. Therefore, we need
+  // to restrict x to be no lower than 0. Additionally, we need to make sure we don't hit a
+  // divide by zero error, so we add a very small value to the denominator.
+  double chi_s = gammainc(alpha_s, beta_s*max(solnvec[2]/(solnvec[3]+1e-8),0));
+  double chi_e = gammainc(alpha_e, beta_e*max(solnvec[1]/(solnvec[4]+1e-8),0));
+  double chi_p = 0.3;
+
+  // calculate q_s
+  double F_s = (solnvec[2] + solnvec[3])/(solnvec[2] + solnvec[3] + K_s);
+  double F_e = (solnvec[1] + beta_E*solnvec[4])/(solnvec[1] + beta_E*solnvec[4] + K_e);
+  double q_s = qs_max*F_s*F_e;
+
+  // calculate intermediate rates
+  double rar = chi_p*y_as*q_s*X;
+  double rbr = (1-chi_p)*y_bs*qx*X;
+
+  double our = -chi_e*y_os*q_s*X;
+  double otr = kLa *(o2_max - solnvec[1]);
+  double rae = -(1-chi_e)*y_os*q_s*X;
+  double rbe = -rae;
+
+
+  // calculate final rates
+  rhs[0] = y_xs*q_s*solnvec[0]*(1 - solnvec[0]/x_max);
+  rhs[1] = our+otr;
+  rhs[2] = -chi_s     *q_s*solnvec[0];
+  rhs[3] = -(1-chi_s) *q_s*solnvec[0];
+  rhs[4] = rar+rae;
+  rhs[5] = rbr+rbe;
+
 }
 
 void advance(std::vector<double>& solnvec,int nvars,double t_now,double t_adv,double dt)
