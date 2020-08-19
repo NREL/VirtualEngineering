3842b51d56c8c72632d8cd309eff39251c043415
eyoung <ethan.young@nrel.gov>
2020-08-19 14:45:17 -0600

    modified run script for EH CFD

--- EH_CFD/run.sh
+++ EH_CFD/run.sh
@@ -3,10 +3,10 @@ NPROC=$1
 PROBLEMNAME=paddle
 echo ${PROBLEMNAME} > input_genmap
 echo 0.01 >> input_genmap
+export NEK_SOURCE_ROOT=../submodules/Nek5000
+export PATH=${NEK_SOURCE_ROOT}/bin:${PATH}
 genmap < input_genmap
 echo ${PROBLEMNAME} > SESSION.NAME
 echo $PWD >> SESSION.NAME
-export NEK_SOURCE_ROOT=../submodules/Nek5000
-export PATH=${NEK_SOURCE_ROOT}/bin:${PATH}
 makenek paddle
 mpirun -n ${NPROC} ./nek5000
