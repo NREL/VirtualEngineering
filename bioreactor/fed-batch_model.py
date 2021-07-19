5a9d0008919b9e83b862fece0208dc95ae97b70f
Jonathan Stickel <jjstickel@gmail.com>
2021-07-19 09:53:14 -0600

    add sugar depletion to fed-batch model example

--- bioreactor/fed-batch_model.py
+++ bioreactor/fed-batch_model.py
@@ -34,10 +34,11 @@ f_l = 0.5 # g lipid / g cell, fration DCW that is lipid
 # starting conditions -- these should be provided as input by user
 t0 = 12 # h
 DCW0 = 10 # g/L
+m_s0 = (20 + 70)*0.3 # g, sugar mass 
+# reactor volume
+V = 0.3 # L
 
-# end time should be determined by depletion of substrate in the for loop --
-# not implemented yet, JJS 5/5/21
-t = np.linspace(t0, 72, 10)
+t = np.linspace(t0, 96, 20)
 #t = np.array([t0, 40, 84])
 #t = np.array([t0, 36, 60, 84])
 #t = np.array([t0, 48, 84])
@@ -45,15 +46,23 @@ N = t.size
 OUR = np.zeros(N) # molar oxygen uptake rate
 DCW = np.zeros(N) # dry cell weight (density)
 rho_l = np.zeros(N) # density of lipid
+m_s = np.zeros(N) # mass of sugar feed
 
 #time zero
 DCW[0] = DCW0
 rho_l[0] = f_l*DCW0
+m_s[0] = m_s0
 OUR[0] = fOUR*lmdour*DCW0
 
 # later times
 for i in range(1,N):
-    r_l = OUR[i-1]*nr_l_o*mw_l/1000 # mass rate of lipid production
+    r_s = -OUR[i-1]*nr_g_o*mw_g/1000 # g/(L.h), mass rate of sugar depletion
+    m_s[i] = m_s[i-1] + r_s*V*(t[i] - t[i-1])
+    if m_s[i] > 0:
+        r_l = OUR[i-1]*nr_l_o*mw_l/1000 # g/(L.h), mass rate of lipid production
+    else:
+        r_l = 0 # no lipid production if substrate is depleted
+        m_s[i] = 0
     rho_l[i] = rho_l[i-1] + r_l*(t[i] - t[i-1])
     DCW[i] = rho_l[i]/f_l
     OUR[i] = fOUR*lmdour*DCW[i]
@@ -61,12 +70,13 @@ for i in range(1,N):
 
 figure(1)
 clf()
-plot(t, DCW, 'o-C0', label="DCW")
+plot(t, DCW, '-C0', label="DCW")
 plot(texp, DCWexp, 'o--C0', mfc="none")
-plot(t, rho_l, 's-C1', label="lipid")
+plot(t, rho_l, '-C1', label="lipid")
 plot(texp, lipexp, 's--C1', mfc="none")
-plot(t, OUR, "d-C2", label="OUR")
+plot(t, OUR, "-C2", label="OUR")
+plot(t, m_s, "-C3", label="sugar")
 xlabel("t")
-ylabel("DCW, lipid [g/L]; OUR [mol/($m^3$.h)]")
+ylabel("DCW, lipid [g/L]; OUR [mol/($m^3$.h)]; sugar [g]")
 legend(loc="best")
 #savefig("fed-batch_compare.pdf", bbox_inches="tight")
