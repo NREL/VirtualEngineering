34c22ef6b0d2838c441c44e8aaac1ba08c3519f7
Ethan Young <ethan.young@nrel.gov>
2022-03-04 13:29:45 -0700

    force reinstall of vebio package

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from setuptools import setup
 setup(name = 'Virtual Engineering Toolbox',
     version = '0.0',
     packages = ['vebio'],
-    zip_safe = False)
+    zip_safe = False,
+    package_dir={'vebio': 'vebio/'},
+    package_data={'': ['vebio/*.so']})
